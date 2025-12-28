b593fdc781bc078d3d718f4e3d5bd068a91408fb
Dmitriy Derepko <xepozz@list.ru>
2025-12-28 11:02:46 +0400

    chore: separate classes

---
+++ src/main/kotlin/com/github/xepozz/temporal/common/run/TemporalConfigurationFactory.kt
@@ -0,0 +1,17 @@
+package com.github.xepozz.temporal.common.run
+
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.project.Project
+
+class TemporalConfigurationFactory(type: ConfigurationType) : ConfigurationFactory(type) {
+    override fun createTemplateConfiguration(project: Project): RunConfiguration {
+        return TemporalRunConfiguration(project, this, "Temporal")
+    }
+
+    override fun getOptionsClass(): Class<out BaseState> = TemporalRunConfigurationOptions::class.java
+
+    override fun getId(): String = "TemporalConfigurationFactory"
+}
--- src/main/kotlin/com/github/xepozz/temporal/common/run/TemporalConfigurationType.kt
+++ src/main/kotlin/com/github/xepozz/temporal/common/run/TemporalConfigurationType.kt
@@ -5,9 +5,6 @@ import com.github.xepozz.temporal.TemporalIcons
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.configurations.ConfigurationTypeUtil
-import com.intellij.execution.configurations.RunConfiguration
-import com.intellij.openapi.components.BaseState
-import com.intellij.openapi.project.Project
 import javax.swing.Icon
 
 class TemporalConfigurationType : ConfigurationType {
@@ -30,12 +27,3 @@ class TemporalConfigurationType : ConfigurationType {
     }
 }
 
-class TemporalConfigurationFactory(type: ConfigurationType) : ConfigurationFactory(type) {
-    override fun createTemplateConfiguration(project: Project): RunConfiguration {
-        return TemporalRunConfiguration(project, this, "Temporal")
-    }
-
-    override fun getOptionsClass(): Class<out BaseState> = TemporalRunConfigurationOptions::class.java
-
-    override fun getId(): String = "TemporalConfigurationFactory"
-}
